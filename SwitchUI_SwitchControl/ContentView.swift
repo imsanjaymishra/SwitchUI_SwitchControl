0651616f7eb84714123212d11ade8aa4f3013d7e
Sanjay Mishra <jaysan.mishra@gmail.com>
2019-12-08 01:30:11 +0530

    Initial Commit

---
+++ SwitchUI_SwitchControl/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwitchUI_SwitchControl
+//
+//  Created by Sanjay Mishra on 08/12/19.
+//  Copyright © 2019 Sanjay Mishra. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
